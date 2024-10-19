d3eb9bf239e0354452ec946f74c13b360795f43b
Fernanda <fernanda19@usf.edu>
2024-10-19 16:30:00 -0400

    Task 3: Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -43,10 +43,33 @@ class Section {
     listBooks(){ 
         let bookList = '';
         this.books.forEach(book => {
-          let available = book.isAvailable ? `Available` : `Not Available`; 
+          let available = book.isAvailable ? `Available` : `Not Available`; //checks if book is available 
           bookList += `${book.title}, is ${available}`
     })
     return bookList //will return the list of books
     }
     }
+    // Create a Patron Class
+    class Patron {
+constructor (name, borrowedBooks) {
+this.name = name; 
+this.borrowedBooks = []; 
+}
+borrowBooks(book){
+    if (book.isAvailable) {
+book.isAvailable = false //updates availability
+this.borrowedBooks.push(book) //pushes book into array 
+console.log(`${this.name} borrowed ${book.title}`)
+    } else { console.log(`Book is not available to borrow`)}
+
+}
+returnBook(book){
+   let bookFound = this.borrowedBooks.findIndex(borrowedBook => borrowedBook === book) // finds the book in the borrowed books array 
+    if (bookFound !== -1) { // -1 means it was not found. so if found proceed with code
+        this.borrowedBooks.splice(bookFound, 1) // remove the book from this array 
+    }
+    book.isAvailable = true // makes book available again 
+    console.log(`${this.name} returned ${book.title}`) //logs what happened 
+}
+    }; 
 
